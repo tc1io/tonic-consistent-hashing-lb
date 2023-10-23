ddf7afd7a14c9f6a365e6f23e8cfd99ddeb617e1
ashwinsekaran <ashwin.sekaran@tc1.io>
2023-10-23 18:01:46 +0200

    added k8s client for fetching pods

--- src/client.rs
+++ src/client.rs
@@ -4,6 +4,9 @@ use hello_world::HelloRequest;
 use fasthash::murmur3;
 use prost::Message;
 use std::collections::{BTreeMap, HashMap};
+use k8s_openapi::api::core::v1::Pod;
+use kube::{Client, ResourceExt};
+use kube::api::{Api, ListParams};
 
 // Tonic LB Reference - https://github.com/hyperium/tonic/blob/master/examples/src/load_balance/client.rs
 // Consistent hash reference - https://github.com/zonyitoo/conhash-rs/blob/master/src/conhash.rs
@@ -59,18 +62,24 @@ impl ConsistentHash {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-   let mut ch = ConsistentHash::new();
+   // let mut ch = ConsistentHash::new();
+   //
+   //  let mut nodes = vec![];
+   //  // TODO: Get pods from kubeapi (Check for appropriate crate)
+   //  nodes.push(Node::new("http://0.0.0.0", 8087));
+   //  nodes.push(Node::new("http://0.0.0.0", 8088));
+   //  nodes.push(Node::new("http://0.0.0.0", 8089));
+   //
+   //  for node in nodes {
+   //      ch.add(node, 3);
+   //  }
 
-    let mut nodes = vec![];
-    // TODO: Get pods from kubeapi (Check for appropriate crate)
-    nodes.push(Node::new("http://0.0.0.0", 8087));
-    nodes.push(Node::new("http://0.0.0.0", 8088));
-    nodes.push(Node::new("http://0.0.0.0", 8089));
-
-    for node in nodes {
-        ch.add(node, 3);
+    let k8s_client = Client::try_default().await?;
+    let pods: Api<Pod> = Api::default_namespaced(k8s_client);
+    let lp = ListParams::default().labels("app.kubernetes.io/name=grpc");
+    for p in pods.list(&lp).await? {
+        println!("Pod name: {}", p.name_any());
     }
-
     //let endpoints = ["http://[::1]:8080", "http://[::1]:8081",  "http://[::1]:8082"]
 
     let endpoints = ["http://0.0.0.0:8087", "http://0.0.0.0:8088", "http://0.0.0.0:8089"]
