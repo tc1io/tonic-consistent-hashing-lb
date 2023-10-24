41af1fde3ffec35229e7d14807d6ee50da0bf7f6
ashwinsekaran <ashwin.sekaran@tc1.io>
2023-10-24 15:20:06 +0200

    get_next_node logic and test it

--- src/client.rs
+++ src/client.rs
@@ -48,7 +48,7 @@ impl ConsistentHash {
         }
     }
 
-    pub fn add(&mut self, node: Node, rep_count: usize) {
+    pub fn add(&mut self, node: &Node, rep_count: usize) {
         let node_name = format!("{}:{}", node.host, node.port);
         self.replicas.insert(node_name.clone(), rep_count);
         for replica in 0..rep_count {
@@ -57,29 +57,59 @@ impl ConsistentHash {
             self.ring.insert(key, node.clone());
         }
     }
+
+    pub fn get_next_node(&self, k: &str) -> Option<&Node> {
+        let key = k.as_bytes();
+        if self.ring.is_empty() {
+            return None;
+        }
+
+        let hashed_key = create_hash(key);
+
+        let entry = self.ring.range(hashed_key..).next();
+        if let Some((_k, v)) = entry {
+            return Some(v);
+        }
+        let first = self.ring.iter().next();
+        let (_k, v) = first.unwrap();
+        Some(v)
+    }
+
+    pub fn len(&self) -> usize {
+        self.ring.len()
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-   // let mut ch = ConsistentHash::new();
-   //
-   //  let mut nodes = vec![];
-   //  // TODO: Get pods from kubeapi (Check for appropriate crate)
-   //  nodes.push(Node::new("http://0.0.0.0", 8087));
-   //  nodes.push(Node::new("http://0.0.0.0", 8088));
-   //  nodes.push(Node::new("http://0.0.0.0", 8089));
-   //
-   //  for node in nodes {
-   //      ch.add(node, 3);
-   //  }
+   let mut ch = ConsistentHash::new();
 
-    let k8s_client = Client::try_default().await?;
-    let pods: Api<Pod> = Api::default_namespaced(k8s_client);
-    let lp = ListParams::default().labels("app.kubernetes.io/name=grpc");
-    for p in pods.list(&lp).await? {
-        println!("Pod name: {}", p.name_any());
+    let mut nodes = vec![];
+    // TODO: Get pods from kubeapi (Check for appropriate crate)
+    nodes.push(Node::new("http://0.0.0.0", 8087));
+    nodes.push(Node::new("http://0.0.0.0", 8088));
+    nodes.push(Node::new("http://0.0.0.0", 8089));
+
+    for node in nodes {
+        ch.add(&node, 3);
     }
+
+    // Test the get logic
+    for j in 0..50usize {
+        let data = format!("hello-{}", j);
+        let next = ch.get_next_node(data.as_str()).unwrap();
+        println!("next {:?}", next);
+    }
+
+    // TODO: Refactor k8s impl below
+    // let k8s_client = Client::try_default().await?;
+    // let pods: Api<Pod> = Api::default_namespaced(k8s_client);
+    //
+    // let lp = ListParams::default().labels("helm.sh/chart=grpc-0.1.0-server");
+    // for p in pods.list(&lp).await? {
+    //     println!("Pod name: {}", p.name_any());
+    // }
     //let endpoints = ["http://[::1]:8080", "http://[::1]:8081",  "http://[::1]:8082"]
 
     let endpoints = ["http://0.0.0.0:8087", "http://0.0.0.0:8088", "http://0.0.0.0:8089"]
