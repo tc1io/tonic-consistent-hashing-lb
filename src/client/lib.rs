9f26d0d6c5d09d8ce3a9fb07257dbd34b2be3b54
ashwinsekaran <ashwin.sekaran@tc1.io>
2023-12-06 14:09:01 +0100

    delay service

--- src/client/lib.rs
+++ src/client/lib.rs
@@ -1,117 +1,138 @@
-use std::convert::Infallible;
-use std::future::{Ready, ready};
-use std::task::{Context, Poll};
-//use tonic::transport::Channel;
-use hello_world::greeter_client::GreeterClient;
-use hello_world::HelloRequest;
-use consistent_hash::ConsistentHash;
-use node::Node;
-use tonic::transport::channel as tonic_channel;
-use tower_service::Service;
-//use crate::ch::DebugService;
-
-pub mod consistent_hash;
-pub mod k8s;
-pub mod node;
-mod ch;
-
-// pub mod channel;
-// pub mod endpoint;
-// pub mod error;
-// pub mod executor;
-// pub mod connection;
-// pub mod grpc_timeout;
-// pub mod reconnect;
-// pub mod user_agent;
-// pub mod add_origin;
-// pub mod dynamicservicestream;
-// mod tonic_service;
+// ----------------------------Greeter client example start-----------------------------------------//
+// use std::convert::Infallible;
+// use std::future::{Ready, ready};
+// use std::task::{Context, Poll};
+// //use tonic::transport::Channel;
+// use hello_world::greeter_client::GreeterClient;
+// use hello_world::HelloRequest;
+// use consistent_hash::ConsistentHash;
+// use node::Node;
+// use tonic::transport::channel as tonic_channel;
+// use tower_service::Service;
+// //use crate::ch::DebugService;
+//
+// pub mod consistent_hash;
+// pub mod k8s;
+// pub mod node;
 // mod ch;
+//
+// type BoxFuture<'a, T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
+// type Error = Box<dyn std::error::Error + Send + Sync>;
+//
+// pub mod hello_world {
+//     tonic::include_proto!("helloworld");
+// }
+//
+// const POD_LABEL: &str = "helm.sh/chart=grpc-server";
+// const PORT_NAME: &str = "grpc-server";
+// const STATEFULSET_NAME: &str = "tonic-consistent-hashing";
+//
+// // Tonic LB Reference - https://github.com/hyperium/tonic/blob/master/examples/src/load_balance/client.rs
+// // Consistent hash reference - https://github.com/zonyitoo/conhash-rs/blob/master/src/conhash.rs
+//
+// #[tokio::main]
+// async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//     let mut ch = ConsistentHash::new();
+//
+//    let nodes: Vec<Node> = ch.get_pods(POD_LABEL, STATEFULSET_NAME, PORT_NAME).await?;
+//
+//     // let mut nodes = vec![];
+//     // nodes.push(Node::new("http://test1", 8087));
+//     // nodes.push(Node::new("http://test2", 8088));
+//     // nodes.push(Node::new("http://test3", 8089));
+//
+//     if !nodes.is_empty() {
+//         for node in nodes.iter() {
+//             println!("Node: Host-{} Port-{}", node.host, node.port);
+//             ch.add(&node);
+//         }
+//     }
+//
+//     // Test the get logic
+//     for j in 0..50usize {
+//         let data = format!("hello-{}", j);
+//         let next = ch.get_next_node(data.as_str()).unwrap();
+//         println!("next {:?}", next);
+//     }
+//
+//     let endpoints = ["http://0.0.0.0:8087", "http://0.0.0.0:8088", "http://0.0.0.0:8089"]
+//         //let endpoints = ["http://10.244.0.205:8086", "http://10.244.0.206:8086", "http://10.244.0.207:8086"]
+//         .iter()
+//         .map(|a| tonic_channel::Channel::from_static(a));
+//
+//     //let channel = channel::Channel::balance_list(endpoints);
+//     let channel = tonic_channel::Channel::balance_list(endpoints);
+//
+//     //let xxx = DebugService{};
+//
+//     //let mut client = GreeterClient::new(xxx);
+//     let mut client = GreeterClient::new(channel);
+//
+//     for _ in 0..10usize {
+//         let request = tonic::Request::new(HelloRequest {
+//             name: "Hello gPRC".into(),
+//         });
+//
+//         let response = client.say_hello(request).await?;
+//
+//         println!("RESPONSE={:?}", response);
+//     }
+//
+//     Ok(())
+// }
+//--------------------------------------Greeter client - Example end-----------------------------//
 
-type BoxFuture<'a, T> = std::pin::Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
-type Error = Box<dyn std::error::Error + Send + Sync>;
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+//---------------------------------------Delay service - Example start---------------------------------//
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use tower::Service;
+use tokio::time::{sleep, Duration};
+//use anyhow::{Result};
+
+
+struct DelayService;
+
+impl Service<&'static str> for DelayService {
+    type Response = tokio::time::Sleep;
+    type Error = std::io::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+
+
+    fn poll_ready(&mut self, _: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: &'static str) -> Self::Future {
+        let delay_duration = Duration::from_secs(2);
+        println!("received request: '{}', delaying for {} seconds", request, delay_duration.as_secs());
+        //self.call(request);
+        //Box::new(sleep(delay_duration))
+        //let conn = tonic::transport::Endpoint::new("http://0.0.0.0:8087")?.connect().await?;
+        let fut = async move{
+            Ok(sleep(delay_duration))
+        };
+
+        Box::pin(fut)
+    }
 }
-
-const POD_LABEL: &str = "helm.sh/chart=grpc-server";
-const PORT_NAME: &str = "grpc-server";
-const STATEFULSET_NAME: &str = "tonic-consistent-hashing";
-
-// Tonic LB Reference - https://github.com/hyperium/tonic/blob/master/examples/src/load_balance/client.rs
-// Consistent hash reference - https://github.com/zonyitoo/conhash-rs/blob/master/src/conhash.rs
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut ch = ConsistentHash::new();
+async fn main() {
+    let mut delay_service = DelayService;
 
-   let nodes: Vec<Node> = ch.get_pods(POD_LABEL, STATEFULSET_NAME, PORT_NAME).await?;
+    for j in 0..10usize {
+        let request = "Hello, Tower Service!";
+        let resp = delay_service.call(request);
 
-    // let mut nodes = vec![];
-    // nodes.push(Node::new("http://test1", 8087));
-    // nodes.push(Node::new("http://test2", 8088));
-    // nodes.push(Node::new("http://test3", 8089));
+        resp.await.expect("TODO: panic message").await;
+        println!("response received from tower - {}, after delay", j);
 
-    if !nodes.is_empty() {
-        for node in nodes.iter() {
-            println!("Node: Host-{} Port-{}", node.host, node.port);
-            ch.add(&node);
-        }
     }
-
-    // Test the get logic
-    for j in 0..50usize {
-        let data = format!("hello-{}", j);
-        let next = ch.get_next_node(data.as_str()).unwrap();
-        println!("next {:?}", next);
-    }
-
-    let endpoints = ["http://0.0.0.0:8087", "http://0.0.0.0:8088", "http://0.0.0.0:8089"]
-        //let endpoints = ["http://10.244.0.205:8086", "http://10.244.0.206:8086", "http://10.244.0.207:8086"]
-        .iter()
-        .map(|a| tonic_channel::Channel::from_static(a));
-
-    //let channel = channel::Channel::balance_list(endpoints);
-    let channel = tonic_channel::Channel::balance_list(endpoints);
-
-    //let xxx = DebugService{};
-
-    //let mut client = GreeterClient::new(xxx);
-    let mut client = GreeterClient::new(channel);
-
-    for _ in 0..10usize {
-        let request = tonic::Request::new(HelloRequest {
-            name: "Hello gPRC".into(),
-        });
-
-        let response = client.say_hello(request).await?;
-
-        println!("RESPONSE={:?}", response);
-    }
-
-    Ok(())
 }
-//
-// pub struct DebugService;
-//
-// impl Service<()> for DebugService {
-//     type Response = ();
-//     type Error = Infallible;
-//     type Future = Ready<Result<Self::Response, Self::Error>>;
-//
-//     fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-//         Ok(()).into()
-//     }
-//
-//     fn call(&mut self, _req: ()) -> Self::Future {
-//         ready(Ok(()))
-//     }
-// }
 
-
-
-
+//------------------------------- Delay Service - Example end------------------------------------------//
 
 
 
